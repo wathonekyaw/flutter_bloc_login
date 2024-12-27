dc4bf16780f4691a62f399c5fde04a74eca1189f
Wa Thone Kyaw <wathonekyaw.x@gmail.com>
2024-12-27 17:08:50 +0630

    this is the first version of login app using bloc

---
+++ lib/login/view/login_page.dart
@@ -0,0 +1,29 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/login_bloc.dart';
+import 'login_form.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const LoginPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: BlocProvider(
+          create: (context) => LoginBloc(
+            authenticationRepository: context.read<AuthenticationRepository>(),
+          ),
+          child: const LoginForm(),
+        ),
+      ),
+    );
+  }
+}
